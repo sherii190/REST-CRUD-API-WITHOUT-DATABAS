48eb7b15b36a609593535484a0d19db9be598ec9
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 07:06:20 +0100

    lagt till crud funktioner

--- routes/users.js
+++ routes/users.js
@@ -6,3 +6,22 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/users.js";
+
+//initialize router
+const router = express.Router();
+
+
+router.get("/", getUser);
+
+router.post("/", createUser);
+
+//params in route;
+router.get("/:id", getID);
+
+//delete
+router.delete("/:id", deleteUser);
+
+//update
+router.patch("/:id", updateUser);
+
+export default router;
