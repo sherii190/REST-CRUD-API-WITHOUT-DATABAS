943be0f656d142a2b09ad12ee493191fba6c20b6
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 07:08:49 +0100

    påbörjat med controllers users.js, uuvid4 inlagt

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+//export all the callback functions for the CRUD operations
+import { v4 as uuidv4 } from "uuid";
+
+let users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 27,
+  },
+];
